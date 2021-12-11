6655dcb2cab94ca5ede453753ab0caa650e61547
cejn_dev_user1 <cejndev@protonmail.com>
2021-12-11 08:42:17 -0600

    fix bios routing spec

--- spec/routing/bios_routing_spec.rb
+++ spec/routing/bios_routing_spec.rb
@@ -2,29 +2,30 @@ require "rails_helper"
 
 RSpec.describe BiosController, type: :routing do
   describe "routing" do
+    let(:slug) { "bio_slug" }
+
     it "routes to #index" do
       expect(get: "/bios").to route_to("bios#index")
     end
 
     it "routes to #show" do
-      expect(get: "/bios/1").to route_to("bios#show", id: "1")
+      expect(get: "/bios/#{slug}").to route_to("bios#show", slug: slug)
     end
-
 
     it "routes to #create" do
       expect(post: "/bios").to route_to("bios#create")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/bios/1").to route_to("bios#update", id: "1")
+      expect(put: "/bios/#{slug}").to route_to("bios#update", slug: slug)
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/bios/1").to route_to("bios#update", id: "1")
+      expect(patch: "/bios/#{slug}").to route_to("bios#update", slug: slug)
     end
 
     it "routes to #destroy" do
-      expect(delete: "/bios/1").to route_to("bios#destroy", id: "1")
+      expect(delete: "/bios/#{slug}").to route_to("bios#destroy", slug: slug)
     end
   end
 end
