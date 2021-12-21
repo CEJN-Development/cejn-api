6b076d58dc5c3933c65b445a78ce57b207f7c4f3
cejn_dev_user1 <cejndev@protonmail.com>
2021-12-21 06:21:21 -0600

    add load dotenv to boot and set cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,19 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  origins = if Rails.env.development?
+              [ENV['CEJN_CLIENT_URL'],
+               "#{ENV['ACTION_MAILER_HOST']}/#{ENV['ACTION_MAILER_PORT']}"].freeze
+            else
+              [ENV['CEJN_CLIENT_URL']].freeze
+            end
+
+  allow do
+    origins origins
+    resource  '*',
+              headers: :any,
+              credentials: true,
+              methods: %i[get post put patch delete options head].freeze
+  end
+end
