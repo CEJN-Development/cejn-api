034428121204191f7228f4089b0b832576b67372
cejn_dev_user1 <cejndev@protonmail.com>
2021-12-27 16:58:22 -0600

    add front end routing and request specs for writers

--- spec/requests/writers_spec.rb
+++ spec/requests/writers_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
-RSpec.describe "Writers", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Writers', type: :request do
+  let(:json) { JSON.parse(response.body) }
+
+  describe 'GET /show' do
+    let!(:writer) { create :writer }
+
+    it 'renders a successful response' do
+      get "/writers/#{writer.slug}", as: :json
+      expect(response).to be_successful
+      expect(json['id']).to eq writer.id
+    end
   end
 end
