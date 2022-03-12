c68c65ae8f842bb49b5f297f2414e1cf0eebfd77
cejn_dev_user1 <cejndev@protonmail.com>
2022-03-12 12:55:14 -0600

    press releases model specs and factory

--- spec/models/press_release_spec.rb
+++ spec/models/press_release_spec.rb
@@ -17,5 +17,29 @@
 require 'rails_helper'
 
 RSpec.describe PressRelease, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:press_release) { create :press_release }
+
+  it 'is valid with valid attributes' do
+    expect(press_release).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    press_release.title = nil
+    expect(press_release).not_to be_valid
+  end
+
+  it 'is not valid without a body' do
+    press_release.body = nil
+    expect(press_release).not_to be_valid
+  end
+
+  it 'is not valid without an summary' do
+    press_release.summary = nil
+    expect(press_release).not_to be_valid
+  end
+
+  it 'slug is updated after save' do
+    press_release.update! title: 'A Fancy New Title'
+    expect(press_release.slug).to eq 'a-fancy-new-title'
+  end
 end
