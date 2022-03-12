04466504723b2f0e5a63c1114576e55fd560d140
cejn_dev_user1 <cejndev@protonmail.com>
2022-03-12 12:52:10 -0600

    add model validations and lifecycle callbacks

--- app/models/press_release.rb
+++ app/models/press_release.rb
@@ -15,4 +15,25 @@
 #  index_press_releases_on_slug  (slug)
 #
 class PressRelease < ApplicationRecord
+  validates :title, uniqueness: true
+  validates :title, presence: true, length: { maximum: 70 }
+  validates :summary, presence: true, length: { maximum: 200 }
+  validates :body, presence: true
+
+  before_save :sanitize_body
+  before_save :set_slug
+
+  def sanitize_body
+    self.body = ActionController::Base.helpers.sanitize(body, tags: %w[a b], attributes: %w[href target])
+  end
+
+  def set_slug
+    self.slug = make_slug
+  end
+
+  private
+
+  def make_slug
+    title.parameterize
+  end
 end
