c4346c9b2a7b9e86ebdfdbb45ac80e0cc71b78d2
cejn_dev_user1 <cejndev@protonmail.com>
2022-04-09 18:58:46 -0500

    twitter controller timeline response with options

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,24 @@
 
 # Controller for interacting with twitter API
 class TweetsController < ApplicationController
-  def timeline(options = {})
-    render json: JSON.parse(TwitterApi.timeline(options))
+  def timeline
+    render json: JSON.parse(TwitterApi.new.timeline(timeline_options))
+  end
+
+  private
+
+  def timeline_options
+    {}.merge timeline_params
+  end
+
+  def timeline_params
+    params.permit %i[
+      contributor_details
+      count
+      exclude_replies
+      include_rts
+      max_id
+      since_id
+    ]
   end
 end
