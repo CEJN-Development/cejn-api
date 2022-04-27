f8522b960e0ef9e3079964a32f17318b01d01a1c
cejn_dev_user1 <cejndev@protonmail.com>
2022-04-27 13:31:12 -0500

    events migration

--- db/migrate/20220427162555_create_events.rb
+++ db/migrate/20220427162555_create_events.rb
@@ -1,8 +1,17 @@
+# frozen_string_literal: true
+
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
-
+      t.datetime :date, null: false
+      t.string :cloudinary_image_url
+      t.string :name, null: false
+      t.string :slug
+      t.text :body
+      t.text :description
       t.timestamps
     end
+
+    add_index :events, :slug
   end
 end
