126d520ad10cdb761ffe2f8fee0fd1f86cc72e16
cejn_dev_user1 <cejndev@protonmail.com>
2022-06-11 10:40:14 -0500

    dont require short name

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,6 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :full_name, presence: true
-  validates :short_name, presence: true
 
   def for_display
     {
