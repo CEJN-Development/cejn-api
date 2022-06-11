73f0b59e46c16f2e8e44d5c7757963da2820b86b
cejn_dev_user1 <cejndev@protonmail.com>
2022-06-11 08:41:23 -0500

    require user for registration create

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,16 +1,21 @@
 # frozen_string_literal: true
 
 class RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_user!, only: %i[create update destroy]
+
   respond_to :json
 
   # POST /users
-  # Specs No
   def create
+    unless current_user
+      return render json: { message: 'You are not authorized to create users.' }, status: :unauthorized
+    end
+
     build_resource(sign_up_params)
 
     resource.save
     if resource.persisted?
-      render json: { message: I18n.t('controllers.registrations.confirm') }
+      render json: { message: I18n.t('controllers.registrations.confirm') }, status: :created
     else
       render json: resource.errors, status: 401
     end
